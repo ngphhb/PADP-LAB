ab3a6e650d05f218ca54bf0119431e5c2ca1e244
Harish <harish.s.pukale123@gmail.com>
2021-10-13 20:08:41 +0530

    1

--- 1.c
+++ 1.c
@@ -4,25 +4,27 @@
 
 int main(){
 
-	int row = 500, col = 500, i, j, count=0, sum =0, k;
+	int dim, i, j, count=0, sum =0, k;
+	printf("\n Enter the dimensions : ");
+	scanf("%d",&dim);
 
-	int **arr1 = (int **)malloc(row * sizeof(int *));
+	int **arr1 = (int **)malloc(dim * sizeof(int *));
 
-	for (i=0; i<row; i++)
-		arr1[i] = (int *)malloc(col * sizeof(int));
-	int **arr2 = (int **)malloc(row * sizeof(int *));
-	for (i=0; i<row; i++)
-		arr2[i] = (int *)malloc(col * sizeof(int));
-	int **arr3 = (int **)malloc(row * sizeof(int *));
-	for (i=0; i<row; i++)
-		arr3[i] = (int *)malloc(col * sizeof(int));
+	for (i=0; i<dim; i++)
+		arr1[i] = (int *)malloc(dim * sizeof(int));
+	int **arr2 = (int **)malloc(dim * sizeof(int *));
+	for (i=0; i<dim; i++)
+		arr2[i] = (int *)malloc(dim * sizeof(int));
+	int **arr3 = (int **)malloc(dim * sizeof(int *));
+	for (i=0; i<dim; i++)
+		arr3[i] = (int *)malloc(dim * sizeof(int));
 
-	for(i = 0;i < row; i++)
-		for(j = 0;j < col; j++)
+	for(i = 0;i < dim; i++)
+		for(j = 0;j < dim; j++)
 			arr1[i][j] = count++;
 
-	for(i = 0;i < row; i++)
-		for(j = 0;j < col; j++)
+	for(i = 0;i < dim; i++)
+		for(j = 0;j < dim; j++)
 			arr2[i][j] = count++;
 	int t;
 	printf("Enter the num of threads : ");
@@ -32,9 +34,9 @@ int main(){
 	double x = omp_get_wtime();
 	omp_set_num_threads(t);
 	#pragma omp parallel for private(j, k)
-	for(i = 0;i < row; i++)
-		for(j = 0;j < col; j++)
-			for(k = 0;k < row; k++)
+	for(i = 0;i < dim; i++)
+		for(j = 0;j < dim; j++)
+			for(k = 0;k < dim; k++)
 				arr3[i][j] += arr1[i][k] * arr2[k][j];  //Main multiplication operation
 
 	double y = omp_get_wtime();
