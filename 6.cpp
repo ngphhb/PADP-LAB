f8b341d360062773f6635d8079df8c8aab2d8dad
Harish Sanjaykumar Pukale <46403164+harishsanjaykumarpukale@users.noreply.github.com>
2021-12-09 10:55:19 +0530

    Create 6.cpp

---
+++ 6.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<omp.h>
+#include<fstream>
+#include<cstring>
+using namespace std;
+
+
+
+int main(){
+  string ws[5] = {"aa","the","an","is","of"};
+  for(int nt=1;nt<=8;nt*=2){
+    cout<< "\n for threads " << nt << endl; 
+    double t = omp_get_wtime();
+    #pragma omp parallel for num_threads(nt)
+    for(int i=0;i<5;i++){
+      int c = 0;
+      fstream f;
+      string w;
+      f.open("words.txt"); //w1.txt w2.txt ...
+      while(f >> w)
+        if(strcmp(ws[i].c_str(),w.c_str())==0)
+          c++;
+        // cout<<"\n" << w << endl;
+      cout<<"\n"<<ws[i]<<" "<<c<<" Time :" << omp_get_wtime()-t << endl;
+    }
+    
+  }
+  return 0;
+}
